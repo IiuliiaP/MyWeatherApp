311a93a5c72c1822420646139951dde83cf81fe0
IiuliiaP <joliesky13z@gmail.com>
2023-02-21 17:47:22 +0300

    урок 2, MVVM,добавлен binding и рандомный вывод сообщений на экран

--- app/src/main/java/com/example/myweatherapp/MainActivity.kt
+++ app/src/main/java/com/example/myweatherapp/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.myweatherapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.myweatherapp.view.WeatherDetails
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        if(savedInstanceState == null){
+            supportFragmentManager.beginTransaction().replace(R.id.fragment_container, WeatherDetails.newInstance())
+                .commit()
+        }
     }
 }
---
+++ app/src/main/java/com/example/myweatherapp/viewmodel/WeatherDetailsViewModel.kt
@@ -0,0 +1,25 @@
+package com.example.myweatherapp.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import java.lang.Thread.sleep
+
+class WeatherDetailsViewModel(private val liveData: MutableLiveData<AppState> = MutableLiveData()) : ViewModel() {
+
+    fun getData() : LiveData<AppState> {
+        return liveData
+    }
+    fun getWeather(){
+        Thread{
+            liveData.postValue((AppState.Loading))
+            sleep(2000L)
+            if((0..10).random()> 5){
+            liveData.postValue(AppState.Success(Any()))}
+            else{
+                liveData.postValue(AppState.Error(IllegalAccessError("ошибка")))
+            }
+        }.start()
+    }
+
+}
